814d82b440fb48dfb19f43cce4b47d452b369014
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-06-30 13:20:18 +0300

    Remove GetByStatus feature and add user status query
    - Removed folder reference for `Features\Users\Queries\GetByStatus` in the project file.
    - Added `GetUserByStatusModule` to handle GET requests for users by status.
    - Introduced `GetUserByStatusQuery` class with logging, caching, and rate limiting capabilities.
    - Created `GetUserByStatusResponse` class to structure the response data for user details.

---
+++ Moongazing.Clinicly/src/Moongazing.Clinicly.Application/Features/Users/Queries/GetByStatus/GetUserByEmailModule.cs
@@ -0,0 +1,27 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Moongazing.Clinicly.Application.Features.Users.Queries.GetByEmail;
+
+namespace Moongazing.Clinicly.Application.Features.Users.Queries.GetByStatus;
+
+public class GetUserByStatusModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/clinicly/users/by-status", async ([AsParameters] GetUserByStatusQuery request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var query = new GetUserByStatusQuery { Status = request.Status };
+            var result = await sender.Send(query, cancellationToken);
+            return Results.Ok(result);
+        })
+        .WithName("GetUserByStatus")
+        .WithTags("Users")
+        .Produces<GetUserByStatusResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get user by status")
+        .WithDescription("Returns a user's details using their status via query string.");
+    }
+}
---
+++ Moongazing.Clinicly/src/Moongazing.Clinicly.Application/Features/Users/Queries/GetByStatus/GetUserByStatusQuery.cs
@@ -0,0 +1,56 @@
+﻿using Mapster;
+using MediatR;
+using Moongazing.Clinicly.Application.Features.Users.Constants;
+using Moongazing.Clinicly.Application.Features.Users.Rules;
+using Moongazing.Clinicly.Application.Repositories;
+using Moongazing.Clinicly.Domain.Enums;
+using Moongazing.Kernel.Application.Pipelines.Authorization;
+using Moongazing.Kernel.Application.Pipelines.Caching;
+using Moongazing.Kernel.Application.Pipelines.Logging;
+using Moongazing.Kernel.Application.Pipelines.Performance;
+using Moongazing.Kernel.Application.Pipelines.RateLimiting;
+using Moongazing.Kernel.Application.Responses;
+using Moongazing.Kernel.Security.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moongazing.Clinicly.Application.Features.Users.Queries.GetByStatus;
+
+public class GetUserByStatusQuery : IRequest<PaginatedResponse<GetUserByStatusResponse>>,
+    ILoggableRequest,
+    IIntervalRequest,
+    ICachableRequest,
+    IRateLimitedRequest
+{
+    public UserStatus Status { get; set; } = default!;
+    public string[] Roles => [GeneralOperationClaims.Read];
+    public bool BypassCache { get; }
+    public string? CacheGroupKey => UsersMessages.SectionName;
+    public string CacheKey => $"{GetType().Name}({Status})";
+    public TimeSpan? SlidingExpiration { get; }
+    public int Interval => 15;
+
+    public class GetUserByStatusQueryHandler : IRequestHandler<GetUserByStatusQuery, PaginatedResponse<GetUserByStatusResponse>>
+    {
+        private readonly IUserRepository userRepository;
+        private readonly UserBusinessRules userBusinessRules;
+        public GetUserByStatusQueryHandler(IUserRepository userRepository, UserBusinessRules userBusinessRules)
+        {
+            this.userRepository = userRepository;
+            this.userBusinessRules = userBusinessRules;
+        }
+        public async Task<PaginatedResponse<GetUserByStatusResponse>> Handle(GetUserByStatusQuery request, CancellationToken cancellationToken)
+        {
+            var users = await userRepository.GetListAsync(predicate: u => u.Status == request.Status,
+                                                          withDeleted: false,
+                                                          cancellationToken: cancellationToken);
+
+            var response = users.Adapt<PaginatedResponse<GetUserByStatusResponse>>();
+            return response;
+        }
+    }
+
+}
